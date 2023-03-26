a1f045bb5315171c1ea1e9467dcba755f736c4ea
Kevin Grimaldi <anthgrim@icloud.com>
2023-03-26 18:26:26 -0400

    adding auth service instance creation test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,13 +1,15 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 it('can create an instance of auth service', async () => {
   // Create fake copy of users service
   const fakeUsersService = {
-    findUserById: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
+    isEmailInUse: async (_email: string) => Promise.resolve(false),
+    create: async (email: string, password: string) =>
       Promise.resolve({ id: 1, email, password }),
+    findUsersByEmail: async (_email: string) => Promise.resolve([]),
   };
 
   const module = await Test.createTestingModule({
