98dfb69e4f5ff4f41f063c20f74f9c260b9cb237
Kevin Grimaldi <anthgrim@icloud.com>
2023-03-26 18:38:18 -0400

    fixing types annotations

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,15 +1,16 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 it('can create an instance of auth service', async () => {
   // Create fake copy of users service
-  const fakeUsersService = {
+  const fakeUsersService: Partial<UsersService> = {
     isEmailInUse: async (_email: string) => Promise.resolve(false),
-    create: async (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
     findUsersByEmail: async (_email: string) => Promise.resolve([]),
-  } as UsersService;
+    create: async (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password } as User),
+  };
 
   const module = await Test.createTestingModule({
     providers: [
